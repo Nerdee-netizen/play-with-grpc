6871553d02bde2c9083e5b68899793dd304af7e7
Zheng Zhe <zhe.zheng@motiong.ai>
2024-07-31 06:58:21 +0000

    update .gitignore

--- app/services/server.py
+++ app/services/server.py
@@ -3,18 +3,21 @@ from concurrent import futures
 import sum_pb2
 import sum_pb2_grpc
 
+
 class SumService(sum_pb2_grpc.SumServiceServicer):
     def Sum(self, request, context):
         result = request.num1 + request.num2
         return sum_pb2.SumResponse(result=result)
 
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     sum_pb2_grpc.add_SumServiceServicer_to_server(SumService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port("[::]:50051")
     server.start()
     print("Server started, listening on port 50051")
     server.wait_for_termination()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     serve()
